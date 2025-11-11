b9fda4aa89230bcf62f055124931cadd376d0a63
Isabelle Lopes <isabelle.silops@gmail.com>
2025-11-11 15:51:53 -0300

    config

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [msg, setMsg] = useState('');
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then((r) => r.json())
-      .then((data) => setMsg(data.message))
-      .catch(console.error);
-  }, []);
+  const [count, setCount] = useState(0)
 
   return (
-    <div style={{padding:20}}>
-      <h1>Frontend React + Vite</h1>
-      <p>Mensagem do backend: {msg || 'carregando...'}</p>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
